ac1686bf0e57b7b8ab00ecd6263094697013f892
ahmedzitouni586 <zitouniahmed595@gmail.com>
2021-09-17 07:40:56 -0700

    CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" script that returns information about employees in CSV format"""
+
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
+
+    USER_ID = argv[1]
+    r = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                     format(USER_ID)).json()
+    USERNAME = r.get("username")
+    r = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                     format(USER_ID)).json()
+    with open(USER_ID + ".csv", "w") as csvfile:
+        f = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for i in r:
+            f.writerow([argv[1], USERNAME, i.get("completed"), i.get("title")])
